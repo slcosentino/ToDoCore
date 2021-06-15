c4659fbba40b8401b2968dec34dcb3281d8a1eea
SEBASTIAN COSENTINO <SCOSENTINO@iram.org.ar>
2021-06-15 14:36:52 -0300

    enable swagger

--- ToDo.API/Startup.cs
+++ ToDo.API/Startup.cs
@@ -81,12 +81,12 @@ namespace ToDo.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo.API v1"));
-            }
+           // }
 
             app.UseCors();
 
